3804cea178144220551bfa6e27bc7ef1d3b82557
Aji <febriatmoko@gmail.com>
2021-01-04 10:07:46 +0700

    add basic signals

--- basic_signals.py
+++ basic_signals.py
@@ -3,7 +3,7 @@ import numpy as np
 def impseq(n0, n1, n2):
     ''' Generates x(n) = delta(n-n0)
     n1 <= n <= n2 '''
-    n = np.array([i for i in range(n1, n2+1)])
+    n = np.array(range(n1, n2+1))
     x = np.zeros((len(n),))
     x[(n-n0)==0] = 1
 
@@ -13,7 +13,27 @@ def impseq(n0, n1, n2):
 #print(n)
 #print(x)
 
-def stepseq
+def stepseq(n0, n1, n2):
+    ''' Generates x(n) = u(n-n0) 
+    n1 <= n <= n2'''
+    n = np.array(range(n1, n2+1))
+    x = np.zeros((len(n),))
+    x[(n-n0)>=0] = 1
+
+    return x, n
+
+#x, n = stepseq(0, -3, 4)
+#print(n)
+#print(x)
+
+# Exponential sequence
+n1 = 0
+n2 = 10
+n = np.array(range(n1, n2+1))
+x = np.power(0.9, n)
+print(n)
+print(x)
+
 
 
 
